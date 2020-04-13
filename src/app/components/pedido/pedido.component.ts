35b901f59c053dcfce73150145a3996d0ea46088
murayaminha <LMMannocci@raiadrogasil.com>
2020-04-13 11:47:05 -0300

    listar pedidos

--- src/app/components/pedido/pedido.component.ts
+++ src/app/components/pedido/pedido.component.ts
@@ -47,9 +47,15 @@ return {
    
     this.mostrarPedidos()
   }
-
-
- 
+  dets : any [] = []
+  posicao: any 
+  details(pedido) {
+      this.dets=[]
+      this.detalhe
+      this.posicao = this.pedido.indexOf(pedido), console.log(this.posicao)  
+      this.dets.push(pedido)
+      console.log(this.dets)
+  }
 
 }
 
