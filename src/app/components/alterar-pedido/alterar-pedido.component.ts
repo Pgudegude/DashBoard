d8bd20927341d29fdd93d3313011ef0bf3ac20d2
murayaminha <LMMannocci@raiadrogasil.com>
2020-04-13 10:16:21 -0300

    ajustando pedido

--- src/app/components/alterar-pedido/alterar-pedido.component.ts
+++ src/app/components/alterar-pedido/alterar-pedido.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-alterar-pedido',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlterarPedidoComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private fb:FormBuilder) { }
+  alterarUsuario :FormGroup
   ngOnInit(): void {
+    this.criarFormulario()
   }
-
+  criarFormulario(){
+    this.alterarUsuario = this.fb.group({
+      
+    })
+  }
+alterarStatus(){
+  console.log("estou alterando")
+}
 }
