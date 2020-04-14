bac89aa5e6ed6030f309ac1c8999c8e9819ae18b
Francklin Medeiros Rodrigues <FMRodrigues@raiadrogasil.com>
2020-04-14 11:57:32 -0300

    Cliente alterado atualizado e lista de produtos atualizado

--- src/app/components/listar-produtos/listar-produtos.component.ts
+++ src/app/components/listar-produtos/listar-produtos.component.ts
@@ -1,5 +1,9 @@
+import { Validators } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from 'src/app/service/product.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-listar-produtos',
@@ -9,18 +13,45 @@ import { ProductService } from 'src/app/service/product.service';
 export class ListarProdutosComponent implements OnInit {
 
   produtos: any = []
+  formularioCategoria: FormGroup
+  categoria: number
 
-  listar(){
-    this.service.getProducts().subscribe((prod)=>{
+
+  constructor(private service: ProductService, private fb: FormBuilder) {
+    this.listar()
+  }
+
+  criandoForm() {
+    this.formularioCategoria = this.fb.group({
+      categoria: [
+        '',
+        Validators.compose([
+          Validators.required
+        ])]
+    })
+  }
+
+  listarCategoria() {
+    let id = this.formularioCategoria.value.categoria
+    this.service.listarPorCategoria(id).subscribe((categoria) => {
+      if (id == 11 ) {
+        this.listar()
+      } else {
+        this.produtos = categoria
+      }
+    })
+  }
+
+
+  listar() {
+    this.service.getProducts().subscribe((prod) => {
       this.produtos = prod
     })
   }
 
-  constructor( private service: ProductService) { 
-    this.listar()
-  }
 
-  ngOnInit(){
+  ngOnInit() {
+    this.criandoForm()
   }
 
 }
--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Endereco } from '../model/endereco';
+import { Observable } from 'rxjs';
+import { retry } from 'rxjs/operators';
+
+const urlAPI: string = 'http://viacep.com.br/ws/';
+
+interface viacep {
+  cep: string,
+  logradouro: string,
+  bairro: string,
+  uf: string,
+  localidade: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  getCep(endereco: Endereco): Observable<viacep> {
+    console.log(endereco._cep);
+    return this.http.get<viacep>(urlAPI + endereco._cep + "/json/").pipe(retry(2));
+  }
+  
 }
